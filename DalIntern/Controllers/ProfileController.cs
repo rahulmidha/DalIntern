36366ba014ad5d878ed4920826d6da4278ac05ec
Nishanth <jebestinishanth@gmail.com>
2018-07-24 18:47:08 -0300

    Develop Commit

---
+++ DalIntern/Controllers/ProfileController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DalIntern.Models;
+using Microsoft.AspNet.Identity;
+
+namespace DalIntern.Controllers
+{
+    [Authorize]
+    public class ProfileController : Controller
+    {
+        const string unknown = "unknown";
+        // GET: Profile
+        public ActionResult Index(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                using (ApplicationDbContext dbContext = new ApplicationDbContext())
+                {
+                    //Get the Current user record from Database
+                    var currentId = User.Identity.GetUserId();
+                    var user = dbContext.Users.Where(x => x.Id == currentId).FirstOrDefault();
+                    if(user!=null)
+                    {
+                        ProfileViewModel viewModel = new ProfileViewModel()
+                        {
+                            Id = user.Id,
+                            DateOfBirth = user.DateOfBirth != null ? user.DateOfBirth.ToString() : unknown,
+                            FirstName = user.FirstName,
+                            LastName  = user.LastName,
+                            Email = user.Email,
+                            EmailConfirmed = user.EmailConfirmed?"Yes":"No",
+                            Anonymous = user.Anonymous ? "Yes" : "No",
+                            Gender = user.Gender=="M"?"Male":"Female",
+                            UserName = user.UserName,
+                            PhoneNumber = user.PhoneNumber!=null? user.PhoneNumber.ToString():unknown                           
+                        };
+                        return View(viewModel);
+                    }                   
+                }
+            }
+
+            return View("Error", new HandleErrorInfo(new Exception("Access is denied."), string.Empty, string.Empty));
+        }
+
+        public ActionResult ShowProfile(string id)
+        {
+            if (!string.IsNullOrEmpty(id))
+            {
+                using (ApplicationDbContext dbContext = new ApplicationDbContext())
+                {
+                    var user = dbContext.Users.Where(x => x.Id == id).FirstOrDefault();
+                    if (user != null && user?.Anonymous == false)
+                    {
+                        ProfileViewModel viewModel = new ProfileViewModel()
+                        {
+                            Id = user.Id,
+                            DateOfBirth = user.DateOfBirth != null ? user.DateOfBirth.ToString() : unknown,
+                            FirstName = user.FirstName,
+                            LastName = user.LastName,
+                            Email = user.Email,
+                            EmailConfirmed = user.EmailConfirmed ? "Yes" : "No",
+                            Anonymous = user.Anonymous ? "Yes" : "No",
+                            Gender = user.Gender == "M" ? "Male" : "Female",
+                            UserName = user.UserName,
+                            PhoneNumber = user.PhoneNumber != null ? user.PhoneNumber.ToString() : unknown
+                        };
+                        return View(viewModel);
+                    }
+                }
+            }
+            return View("Error", new HandleErrorInfo(new Exception("Access is denied."), nameof(ProfileController), "Show Profile"));
+        }
+
+    }
+}
